63e762bdde7d825df66008727edafdefc23defde
Mel Macario <xmelmacario@gmail.com>
2024-05-06 10:50:44 +0800

    added show alert componennt

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -16,6 +16,7 @@ class MyButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialButton(
+      shape: const StadiumBorder(),
       onPressed: () => onPressed(),
       color: color,
       child: Padding(
